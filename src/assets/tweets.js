fdea318a9a06bc4a56cfbecccd8f0a175e8d5247
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-02 18:28:31 -0300

    Mocks de usuários e tweets

---
+++ src/assets/tweets.js
@@ -0,0 +1,32 @@
+const tweets = [
+    {
+        username: "advogadopaloma",
+        tweet: "Bandindinhos!"
+    },
+    {
+        username: "paolabracho",
+        tweet: "Usurpar que é bom, ninguém quer..."
+    },
+    {
+        username: "advogadopaloma",
+        tweet: "Bandindinha!"
+    },
+    {
+        username: "advogadopaloma",
+        tweet: "Eu, Advogado Paloma, estou aqui na Berlin, no último andar, para fazer JUSTIÇA!"
+    },
+    {
+        username: "advogadopaloma",
+        tweet: "Porque EU sou o paladino da Lei!"
+    },
+    {
+        username: "ednaldopereira",
+        tweet: "Você não vale nada."
+    },
+    {
+        username: "vulgomachadão",
+        tweet: "Matamos o tempo, o tempo nos enterra..."
+    }
+];
+
+export default tweets;
---
+++ src/assets/users.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        username: 'advogadopaloma', 
+        avatar: "https://pbs.twimg.com/profile_images/1565376648404471809/lqVzL7a3_400x400.jpg" 
+    },
+    {
+        username: 'paolabracho', 
+        avatar: "https://pt.wikipedia.org/wiki/Paola_Bracho#/media/Ficheiro:La_usurpadora_paola.jpeg" 
+    },
+    {
+        username: 'ednaldopereira', 
+        avatar: "https://i1.sndcdn.com/artworks-U9iTloPpZSWUSWNh-4Mi5nw-t500x500.jpg" 
+    },
+    {
+        username: 'vulgomachadao', 
+        avatar: "https://cdn.brasildefato.com.br/media/d3e65c869db03180a7d34b1569ccdc04.jpg" 
+    }
+];
+
+export default users;
