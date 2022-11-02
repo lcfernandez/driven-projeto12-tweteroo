e0f1d6cf6890535b48d986c9cee7696e6b24da14
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-02 19:00:01 -0300

    Implementação da requisição get /tweets

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 
+import tweets from "./assets/tweets.js"
+import users from "./assets/users.js"
+
 const app = express();
 app.use(cors());
+
+app.get('/tweets', (_, res) => {
+    const tweetsWithAvatar = tweets.map(tweetWithoutAvatar => {
+        const tweetUser = users.find(user => user.username === tweetWithoutAvatar.username);
+        const object = {
+            ...tweetUser,
+            tweet: tweetWithoutAvatar.tweet
+        }
+
+        return object;
+    });
+    res.send(tweetsWithAvatar); 
+});
 
 app.listen(5000, () => {
     console.log("Server running in port: 5000");
