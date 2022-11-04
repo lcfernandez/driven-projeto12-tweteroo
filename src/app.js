0a7aff64ed50a914fa2d63f4f601188d49b79a2b
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-04 11:06:23 -0300

    Implementação da requisiçao post /sign-up

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 import tweets from "./assets/tweets.js"
 import users from "./assets/users.js"
@@ -7,19 +7,23 @@ import users from "./assets/users.js"
 const app = express();
 app.use(cors());
 
-app.get('/tweets', (_, res) => {
+app.get("/tweets", (req, res) => {
     const lastTweets = tweets.slice(-10).reverse();
+    const tweetsWithAvatar = lastTweets.map(tweet => {
+        const tweetUser = users.find(user => user.username === tweet.username);
 
-    const tweetsWithAvatar = lastTweets.map(tweetWithoutAvatar => {
-        const tweetUser = users.find(user => user.username === tweetWithoutAvatar.username);
-        const object = {
+        return {
             ...tweetUser,
-            tweet: tweetWithoutAvatar.tweet
-        }
-
-        return object;
+            tweet: tweet.tweet
+        };
     });
+
     res.send(tweetsWithAvatar);
+});
+
+app.post("/sign-up", (req, res) => {
+    users.push(req.body);
+    res.send("OK");
 });
 
 app.listen(5000, () => {
